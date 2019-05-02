b3e479eaf4cef66a86a6d45edb970ede932498e3
SaeedGitHUBProfile <35467153+SaeedGitHUBProfile@users.noreply.github.com>
2019-05-02 10:17:16 -0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer;
+using Accounting.DataLayer.Repositories;
+using Accounting.DataLayer.Services;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customer=new CustomerRepository();
+
+            //Customers AddCustomer=new Customers()
+            //{
+            //    FullName = "علی محمدی",
+            //    CustomerImage = "NoPhoto",
+            //    Mobile = "09351212125"
+                  
+            //};
+            //customer.InsertCustomer(AddCustomer);
+            //customer.Save();
+
+            var list = customer.GetAllCustomers();
+        }
+    }
+}
